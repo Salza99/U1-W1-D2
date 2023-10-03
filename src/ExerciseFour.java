82a2dbf228997b4a04ac6c61c7546e0fe395f65d
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-10-03 15:11:27 +0200

    exercize n 4 done

--- src/ExerciseFour.java
+++ src/ExerciseFour.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class ExerciseFour {
     public static void main(String[] args) {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("inserisci un numero da cui far partire un countdown");
+        int n1 = input.nextInt();
+
+
+        for (int i=n1; n1 >= 0; n1-- ) {
+                System.out.println(n1);
+            if (n1 == 0) {
+                input.close();
+            }
+
+        }
 
     }
 
