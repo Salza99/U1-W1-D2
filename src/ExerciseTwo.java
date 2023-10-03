17906e5ccbd9f095a636ae5a220227e707fc4d2b
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-10-03 13:59:00 +0200

    exercise 2 completed

--- src/ExerciseTwo.java
+++ src/ExerciseTwo.java
@@ -2,7 +2,26 @@ public class ExerciseTwo {
     public static void main(String[] args) {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println(stampaInLettere(2));
 
+    }
+    public static String stampaInLettere (int n1) {
+        switch (n1) {
+            case 0: {
+                return "zero";
+            }
+            case 1: {
+                return "uno";
+            }
+            case 2: {
+                return "due";
+            }
+            case 3: {
+                return "tre";
+            }
+            default:{
+                return "numero maggiore di 3";
+            }
+        }
     }
 }
