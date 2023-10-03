6edc44f24494557919a011671c3cda505e353c6a
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-10-03 14:59:19 +0200

    try to make exercise 3

--- src/ExerciseThree.java
+++ src/ExerciseThree.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
 public class ExerciseThree {
     public static void main(String[] args) {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("inserisci una stringa per dividerla tra virgole");
+        String stringaInserita = input.nextLine();
+        String close = ":q";
+
+
+
+
+       while (stringaInserita != close) {
+
+           for (int i = 0; i <stringaInserita.length() ; i++) {
+               String newString = stringaInserita.charAt(i) + ",";
+
+               System.out.println(newString);
+
+           }
+            System.out.println("inserisci una nuova stringa oppure digita :q per interrompere");
+
+            stringaInserita = input.nextLine();
+            if (stringaInserita == close) {
+                input.close();
+            }
+
+        }
 
     }
+
 }
