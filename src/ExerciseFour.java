2f8e9cb5212afc7401514ae96c7beace15147668
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-10-03 16:44:00 +0200

    succed to make ex 3

--- src/ExerciseFour.java
+++ src/ExerciseFour.java
@@ -4,7 +4,7 @@ public class ExerciseFour {
     public static void main(String[] args) {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        
+
         Scanner input = new Scanner(System.in);
         System.out.println("inserisci un numero da cui far partire un countdown");
         int n1 = input.nextInt();
--- src/ExerciseThree.java
+++ src/ExerciseThree.java
@@ -4,7 +4,7 @@ public class ExerciseThree {
     public static void main(String[] args) {
         // Press Alt+Invio with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+
         Scanner input = new Scanner(System.in);
         System.out.println("inserisci una stringa per dividerla tra virgole");
         String stringaInserita = input.nextLine();
@@ -13,21 +13,23 @@ public class ExerciseThree {
 
 
 
-       while (stringaInserita != close) {
-
+       while (!stringaInserita.equals(close) ) {
+           String newString = "";
            for (int i = 0; i <stringaInserita.length() ; i++) {
-               String newString = stringaInserita.charAt(i) + ",";
+                newString += stringaInserita.charAt(i) + ",";
 
-               System.out.println(newString);
 
-           }
+
+           }System.out.println(newString);
             System.out.println("inserisci una nuova stringa oppure digita :q per interrompere");
 
             stringaInserita = input.nextLine();
-            if (stringaInserita == close) {
-                input.close();
-            }
 
+
+        }
+       if (stringaInserita == close) {
+           
+            input.close();
         }
 
     }
